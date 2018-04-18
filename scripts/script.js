0ad5b8c9f1b1816ce6bd8bac4eb78676177ba273
Samuel Newhouse <samuel_newhouse@yahoo.com>
2018-04-18 17:07:44 -0500

    Begins rework

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,39 @@
-$(document).ready(function() {
-  $("#userSearchInp").keypress( function(e) {
-    if (e.which=="13")
+$(function () {
+  $("#search-input").keypress(function (e) {
+    if (e.which == "13")
       doSearch();
   });
-  $("#userSearchBut").on("click", function() {
+  $("#search-button").on("click", function () {
     doSearch();
   });
-  $("#randomSearch").on("click", function() {
+  $("#random-search").on("click", function () {
     doRandomSearch();
   });
-  
+  $("#results").on("click", ".search-title", function () {
+    console.log("search-title");
+    doTitleSearch(this.value);
+  });
+
+
   function getAndDisplay(url) {
-    $("#resultsDiv").html("");
-    var resultsDiv = document.getElementById("resultsDiv");
-    
-    $.getJSON(url, function(result) {
+    $("#results").html("");
+    var resultsDiv = document.getElementById("results");
+
+    $.getJSON(url, function (result) {
       var pages = result.query.pages;
       for (var k in pages) {
-        if (pages.hasOwnProperty(k)) {          
+        if (pages.hasOwnProperty(k)) {
           var title = pages[k].title;
           var extract = pages[k].extract;
           var url = "http://www.wikipedia.com/wiki/" + title;
           var panelHTML =
-            '<div class="outBox">' +
-              '<div class="headingBox">' +
-                '<h3><a href="' + url + '" target="_blank">' + title + "</a></h3>" +
+            '<div class="out-box">' +
+              '<div class="heading-box">' +
+                '<button type="button" class="btn btn-secondary search-title" value="'  + title + '">&larr; search title</button>' +
+                '<h3><a class="extra-hover" href="' + url + '" target="_blank">' + title + "</a></h3>" +                
               "</div>" +
-              '<div class="bodyBox">' + extract +            
+              '<div class="body-box">' +
+                extract +
               '</div>';
             '</div>';
           resultsDiv.insertAdjacentHTML("beforeend", panelHTML);
@@ -34,24 +41,35 @@ $(document).ready(function() {
       }
     });
   }
-  
+
   function doSearch() {
-    if( !$("#userSearchInp").val() )
-      return;    
+    if (!$("#search-input").val())
+      return;
     var url =
       "https://en.wikipedia.org/w/api.php" +
       "?format=json&action=query&generator=search&prop=extracts" +
       "&gsrlimit=10&pilimit=max&exintro&explaintext&exchars=500&exlimit=max" +
-      "&origin=*&gsrsearch=" + $("#userSearchInp").val();
+      "&origin=*&gsrsearch=" + $("#search-input").val();
     getAndDisplay(url);
   }
-  
-  function doRandomSearch() {  
+
+  function doRandomSearch() {
     var url =
       "https://en.wikipedia.org/w/api.php" +
       "?format=json&action=query&generator=random&grnnamespace=0&prop=extracts" +
       "&grnlimit=10&pilimit=max&exintro&explaintext&exchars=500&exlimit=max" +
       "&origin=*";
+    getAndDisplay(url);
+  }
+
+  function doTitleSearch(value) {
+    if (!value)
+      return;
+    var url =
+      "https://en.wikipedia.org/w/api.php" +
+      "?format=json&action=query&generator=search&prop=extracts" +
+      "&gsrlimit=10&pilimit=max&exintro&explaintext&exchars=500&exlimit=max" +
+      "&origin=*&gsrsearch=" + value;
     getAndDisplay(url);
   }
 });
